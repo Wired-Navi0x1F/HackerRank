f35df308fef48339da50efe27d1e6108d70b002f
Wired-Navi0x1F <harshulkala14@gmail.com>
2026-07-15 22:53:40 +0530

    Java_End_Of_File.java

---
+++ Java/Java_End_Of_File.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java_End_Of_File {
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner in = new Scanner(System.in);
+        int s = in.hasNext()
+    }
+}
