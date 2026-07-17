a9b02f41a4d2112a4d7f474a25ef1d076c753764
Wired-Navi0x1F <harshulkala14@gmail.com>
2026-07-17 20:07:09 +0530

    Java_End_Of_File.java

--- Java/Java_End_Of_File.java
+++ Java/Java_End_Of_File.java
@@ -6,11 +6,16 @@ import java.util.regex.*;
 
 public class Java_End_Of_File {
     public static void main(String[] args) {
-        /*
-         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
-         * class should be named Solution.
-         */
-        Scanner in = new Scanner(System.in);
-        int s = in.hasNext()
+
+        Scanner sc = new Scanner(System.in);
+        int line = 1;
+
+        while (sc.hasNext()) {
+            String text = sc.nextLine();
+            System.out.println(line + " " + text);
+            line++;
+        }
+
+        sc.close();
     }
 }
